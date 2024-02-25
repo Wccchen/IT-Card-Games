6a9c07ca292145599258360da83afcc53367373c
Wccchen <149198631+Wccchen@users.noreply.github.com>
2024-02-25 17:56:19 +0000

    conflict

---
+++ ITSD-DT2023-24-Template/app/events/EndTurnClicked.java
@@ -0,0 +1,64 @@
+package events;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import akka.actor.ActorRef;
+import commands.BasicCommands;
+import structures.GameState;
+import structures.basic.AI;
+import structures.basic.MoveableUnit;
+import structures.basic.Player;
+import structures.basic.Tile;
+import structures.basic.Unit;
+
+/**
+ * Indicates that the user has clicked an object on the game canvas, in this case
+ * the end-turn button.
+ * 
+ * { 
+ *   messageType = “endTurnClicked”
+ * }
+ * 
+ * @author Dr. Richard McCreadie
+ *
+ */
+public class EndTurnClicked implements EventProcessor{
+
+	@Override
+	public void processEvent(ActorRef out, GameState gameState, JsonNode message) {
+		//remove player 1's unspent mana
+		gameState.getPlayer1().setMana(0, out);
+		//insert AI method where AI makes moves
+<<<<<<< HEAD
+		AI artificial = (AI) gameState.getPlayer2();
+		Tile avatartile = gameState.getBoard().getTile(8, 3);
+		MoveableUnit avatar = avatartile.getUnit();
+		Unit avatarUnit = avatar.getUnit();
+		artificial.aiMoved(out, avatarUnit, avatartile);
+		avatar.moveUnit(out, avatartile, gameState);
+=======
+		//AI artificial = (AI) gameState.getPlayer2(); 
+		//artificial.aiMoved(out,gameState.getPlayer2().);
+//		System.out.println("end turn clicked");
+>>>>>>> a84f6fc038750721b17cd4a04c3956fcbea566cb
+		//remove player 2's unspent mana
+		gameState.getPlayer2().setMana(0, out);
+		//insert draw card for each player
+		gameState.getPlayer1().drawCard(out);
+		gameState.getPlayer2().drawCard(out);
+		gameState.setTurnNumber(gameState.getTurnNumber()+1); //increase turnNumber
+		//increase both players mana by (turn number+1)
+		gameState.getPlayer1().setMana(gameState.getTurnNumber()+1, out);
+		gameState.getPlayer2().setMana(gameState.getTurnNumber()+1, out);
+		String nextTurnMessage = "It's now turn " + gameState.getTurnNumber() + "! Make your moves.";
+		BasicCommands.addPlayer1Notification(out, nextTurnMessage, 2 );
+		gameState.setLastMessage(GameState.noEvent);
+
+
+
+
+
+
+	}
+
+}
---
+++ ITSD-DT2023-24-Template/app/structures/GameState.java
@@ -0,0 +1,138 @@
+package structures;
+
+<<<<<<< HEAD
+import structures.basic.MoveableUnit;
+import structures.basic.Player;
+import structures.basic.Board;
+import structures.basic.Tile;
+import structures.basic.AI;
+=======
+import structures.basic.*;
+>>>>>>> a84f6fc038750721b17cd4a04c3956fcbea566cb
+
+/**
+ * This class can be used to hold information about the on-going game.
+ * Its created with the GameActor.
+ * 
+ * @author Dr. Richard McCreadie
+ *
+ */
+public class GameState {
+	private Player player1;
+	private Player player2; //convert to AI class when we make it?
+	private Board board;
+	private int turnNumber = 1;
+	private int lastCardClicked; //gives hand index of card
+	private Tile lastTileClicked;
+	private MoveableUnit lastUnitClicked;
+
+	private String lastMessage = "NoEvent";
+	/*
+	Possible messages:
+	NoEvent : Nothing of consequence
+	SpellCardClicked
+	CreatureCardClicked (creature CARD clicked)
+	FriendlyUnitClicked (friendly avatar, wraithling or creature unit clicked)
+
+
+	 */
+
+	public boolean gameInitalised = false;
+	
+	public boolean something = false;
+
+	public static final String noEvent = "NoEvent";
+	public static final String spellCardClicked = "SpellCardClicked";
+	public static final String friendlyUnitClicked = "FriendlyUnitClicked";
+	public static final String creatureCardClicked = "CreatureCardClicked";
+	private int frontEndUnitID;
+	private int frontEndCardID;
+
+	public GameState() {
+		this.player1 = new Player(true);
+		this.player2 = new AI(false);
+		this.board = new Board();
+	}
+
+	public Player getPlayer1() {
+		return player1;
+	}
+
+	public Player getPlayer2() {
+		return player2;
+	}
+
+	public Board getBoard() {
+		return board;
+	}
+
+	public void setBoard(Board board) {
+		this.board = board;
+	}
+
+	public int getTurnNumber() {
+		return turnNumber;
+	}
+
+	public void setTurnNumber(int turnNumber) {
+		this.turnNumber = turnNumber;
+	}
+
+	public String getLastMessage() {
+		return lastMessage;
+	}
+
+	public void setLastMessage(String lastMessage) {
+		this.lastMessage = lastMessage;
+		System.out.println(this.lastMessage);
+	}
+
+	public boolean isGameInitalised() {
+		return gameInitalised;
+	}
+
+	public void setGameInitalised(boolean gameInitalised) {
+		this.gameInitalised = gameInitalised;
+	}
+
+	public boolean isSomething() {
+		return something;
+	}
+
+	public void setSomething(boolean something) {
+		this.something = something;
+	}
+	public MoveableUnit getLastUnitClicked() {
+		return lastUnitClicked;
+	}
+
+	public void setLastUnitClicked(MoveableUnit lastUnitClicked) {
+		this.lastUnitClicked = lastUnitClicked;
+	}
+
+	public int getLastCardClicked() {
+		return lastCardClicked;
+	}
+
+	public void setLastCardClicked(int lastCardClicked) {
+		this.lastCardClicked = lastCardClicked;
+	}
+
+	public Tile getLastTileClicked() {
+		return lastTileClicked;
+	}
+
+	public void setLastTileClicked(Tile lastTileClicked) {
+		this.lastTileClicked = lastTileClicked;
+	}
+
+	public int getFrontEndUnitID() {
+		this.frontEndUnitID++;
+		return frontEndUnitID;
+	}
+
+	public int getFrontEndCardID() {
+		this.frontEndCardID++;
+		return frontEndCardID;
+	}
+}
