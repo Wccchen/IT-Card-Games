d379a7d6398e7efd99cdd80a7d1babe1bb2729dd
Wang, Ruxuan <2745883W@student.gla.ac.uk>
2024-03-11 04:20:21 +0000

    setted up and created test method for GetAvartar and DrawingCard

--- ITSD-DT2023-24-Template/test/PlayerTest.java
+++ ITSD-DT2023-24-Template/test/PlayerTest.java
@@ -2,22 +2,38 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import org.mockito.Mockito;
 import akka.actor.ActorRef;
+import structures.basic.Avatar;
+import structures.basic.Player;
+import structures.basic.Card;
+import akka.actor.ActorRef;
+import org.mockito.Mockito;
+import akka.actor.ActorRef; 
 
 public class PlayerTest {
 
-    void setUp() {
+    private Player player;
+    private Avatar avatar;
+    private Card card;
 
+    void setUp() {
+        player = new Player(true);
+        avatar = new Avatar(player);
+        card = new Card();
     }
 
     @Test
     public void testGetAvatar() {
-
+        this.avatar = new Avatar(player);
+        player.setAvatar(avatar);
+        assertEquals(avatar, player.getAvatar());
     }
 
 
     @Test
     public void testDrawCard() {
-       
+        Card card = new Card();
+        player.getPlayerDeck().add(card);
+        assertTrue(player.getHand().contains(card));
     }
 
     // More methods and testing might be added
